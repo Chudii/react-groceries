e1b000e333cadfa73ffce8daff2aaf4ce8c418d5
Chudi <chudiibida3@gmail.com>
2022-08-21 23:41:03 -0400

    added all required features & more

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-class GroceryList extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super()
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleLater = this.handleLater.bind(this)
     }
 
     state = {
@@ -11,7 +13,8 @@ class GroceryList extends React.Component {
         brand: '',
         units: '',
         quantity: 0,
-        isPurchased: false
+        isPurchased: false,
+        purchasedLater: false
     }
 
     handleChange = (event) => {
@@ -22,110 +25,131 @@ class GroceryList extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault()
         console.log('you prevented the default')
+        const newItem = {
+            img: this.state.img,
+            item: this.state.item,
+            brand: this.state.brand,
+            units: this.state.units,
+            quantity: this.state.quantity,
+            isPurchased: this.state.isPurchased,
+        }
+        
         this.setState({ 
-            groceries: [{ img: this.state.img, item: this.state.item, brand: this.state.brand, units: this.state.units, quantity: this.state.quantity, isPurchased: false }, ...groceries], 
+            groceries: [newItem, ...this.state.groceries], 
             img: '',
             item: '',
             brand: '',
             units: '',
             quantity: 0,
-            isPurchased: false
+            isPurchased: false,
+            purchasedLater: false
+        })
+    }
+
+    handleRemove = (index, event) => {
+        const newGroceries = this.state.groceries
+        newGroceries.splice(index, 1)
+        
+        this.setState({
+            groceries: newGroceries
+        })
+    }
+
+    handleLater = (event) => {
+        console.log('nice')
+        let list = event.target.parentElement.parentElement.childNodes
+        list.forEach((node) => {
+            if (node.classList.contains('text')) {
+                node.classList.toggle('strike')
+            }
         })
     }
 
     render() {
         return (
             <div className='groceryContainer'>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor='item'>Item</label>
-                    <input type='text' value={this.state.item} onChange={this.handleChange} id='item' />
-                    <br />
-                    <label htmlFor='brand'>Brand</label>
-                    <input type='text' value={this.state.brand} onChange={this.handleChange} id='brand' />
-                    <br />
-                    <label htmlFor='img'>Image</label>
-                    <input type='text' value={this.state.img} onChange={this.handleChange} id='img' />
-                    <br />
-                    <label htmlFor='units'>Units</label>
-                    <input type='text' value={this.state.units} onChange={this.handleChange} id='units' />
-                    <br />
-                    <label htmlFor='quantity'>Quantity</label>
-                    <input type='number' value={this.state.quantity} onChange={this.handleChange} min={0} id='quantity' />
-                    <br />
+                <div className='title'>
+                    <h1>Grocery List</h1>
+                </div>
+
+                <form className='newItem' onSubmit={this.handleSubmit}>
+                    <h3>New Item</h3>
+                    <div className='user-box'>
+                        <label htmlFor='item'>Item</label>
+                        <input type='text' value={this.state.item} onChange={this.handleChange} id='item' />
+                        <br />
+                    </div>
+
+                    <div className='user-box'>
+                        <label htmlFor='brand'>Brand</label>
+                        <input type='text' value={this.state.brand} onChange={this.handleChange} id='brand' />
+                        <br />
+                    </div>         
+
+                    <div className='user-box'>
+                        <label htmlFor='img'>Image</label>
+                        <input type='text' value={this.state.img} onChange={this.handleChange} id='img' />
+                        <br />
+                    </div>             
+
+                    <div className='user-box'>
+                        <label htmlFor='units'>Units</label>
+                        <input type='text' value={this.state.units} onChange={this.handleChange} id='units' />
+                        <br />
+                    </div>
+                    
+                    <div className='user-box'>
+                        <label htmlFor='quantity'>Quantity</label>
+                        <input type='number' value={this.state.quantity} onChange={this.handleChange} min={0} id='quantity' />
+                        <br />
+                    </div>
+                    
+
                     <input type='submit' />
                 </form>
                 <div className='groceryList'>
-                    <div className='legend'>
-                        <h2>Item</h2>
-                        <h2>Brand</h2>
-                        <h2>Unit</h2>
-                        <h2>Qty</h2>
-                    </div>
-                    {this.state.groceries.map(grocery => {
-                        if (!grocery.isPurchased) {
-                            return (
-                                <div className='groceryItem'>
-                                    <img src={grocery.img}/>
-                                    <h3>{grocery.item}</h3>
-                                    <h3>{grocery.brand}</h3>
-                                    <h3>{grocery.units}</h3>
-                                    <h3 className='quantity'>{grocery.quantity}</h3>
-                                    <button>Remove</button>
-                                </div>
-                            )
-                        }
-                    })}
+                    <table>
+                        <thead>
+                            <th></th>
+                            <th></th>
+                            <th>ITEM</th>
+                            <th>BRAND</th>
+                            <th>UNIT</th>
+                            <th>QTY</th>
+                        </thead>
+                        <tbody>
+                            {this.state.groceries.map((grocery, index) => {
+                                if (!grocery.isPurchased) {
+                                    return (
+                                        <tr>
+                                            <td>
+                                                <button className='laterButton' onClick={this.handleLater}>Buy Later</button>
+                                            </td>
+                                            <td>
+                                                <img src={grocery.img || null}/>
+                                            </td>
+                                            <td className='text'>{grocery.item}</td>
+                                            <td className='text'>{grocery.brand}</td>
+                                            <td className='text'>{grocery.units}</td>
+                                            <td className='quantity text'>{grocery.quantity}</td>
+                                            
+                                            <td>
+                                                <button className='removeButton' onClick={() => this.handleRemove(index)}>Remove</button>
+                                            </td>
+                                            
+                                        </tr>
+                                    )
+                                }
+                            })}
+                        </tbody>
+                    </table>                 
                 </div>
             </div>
         )
     }
 }
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super()
-//         this.handleChange = this.handleChange.bind(this)
-//     }
-
-//     state = {
-//         shoppers: shoppers,
-//         name: '',
-//     }
-
-//     handleChange = (event) => {
-//         console.log(event.target.value)
-//         this.setState({ [event.target.id]: event.target.value })
-//     }
-
-//     handleSubmit = (event) => {
-//         event.preventDefault()
-//         console.log('you prevented the default')
-//     }
-
-//     render() {
-//         return (
-//             <div className='appContainer'>
-//                 <h1>Grocery List App</h1>
-//                 <form onSubmit={this.handleSubmit}>
-//                     <label htmlFor='name'>Name</label>
-//                     <input type='text' value={this.state.name} onChange={this.handleChange} id='name' placeholder='Enter name here' />
-//                     <br />
-//                     <input type='submit' />
-//                 </form>
-//                 {this.state.shoppers.map(shopper => {
-//                     return (
-//                         <>
-//                             <h2>{shopper.name}</h2>
-//                             <GroceryList name={shopper.name} />
-//                         </>
-//                     )
-//                 })}
-//             </div>
-//         )
-//     }
-// }
-
 ReactDOM.render(
-    <GroceryList />,
+    <App />,
     document.querySelector('.container')
 )
--- data.js
+++ data.js
@@ -5,7 +5,8 @@ const groceries = [
         units: '3.7oz',
         quantity: 7,
         img: 'https://bjs.scene7.com/is/image/bjs/28378?$bjs-Zoom$',
-        isPurchased: false
+        isPurchased: false,
+        purchasedLater: false
     },
     {
         item: 'Granola Bars',
@@ -13,7 +14,8 @@ const groceries = [
         units: '35.76oz',
         quantity: 3,
         img: 'https://i5.walmartimages.com/asr/cb06631b-af5b-4da5-875e-e3a4036c87ba.605212a05669840f37a3f270162660b4.jpeg',
-        isPurchased: false
+        isPurchased: false,
+        purchasedLater: false
     },
     {
         item: 'Peanut Butter',
@@ -21,7 +23,8 @@ const groceries = [
         units: '28oz',
         quantity: 5,
         img: 'https://m.media-amazon.com/images/I/71RsrVxeDzL._SL1500_.jpg',
-        isPurchased: false
+        isPurchased: false,
+        purchasedLater: false
     },
 ]
 
